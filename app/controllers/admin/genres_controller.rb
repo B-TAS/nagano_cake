cdd1e6c845e68685e2609f02fe321fa6bcad0f72
ailovemoney <119666255+ailovemoney@users.noreply.github.com>
2023-02-18 10:29:30 +0000

    ADD_genre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,29 @@
 class Admin::GenresController < ApplicationController
+  
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+  
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to repuest.referer
+    else
+      @genres = Genre.all
+      render 'index'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  
 end
