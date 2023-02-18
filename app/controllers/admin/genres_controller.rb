65b12864178e976c1c8d29964e74c44d572b99af
ailovemoney <119666255+ailovemoney@users.noreply.github.com>
2023-02-18 13:03:22 +0000

    [ADD]ジャンル一覧編集

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,10 +1,10 @@
 class Admin::GenresController < ApplicationController
-  
+
   def index
     @genre = Genre.new
     @genres = Genre.all
   end
-  
+
   def create
     @genre = Genre.new(genre_params)
     if @genre.save
@@ -14,16 +14,20 @@ class Admin::GenresController < ApplicationController
       render 'index'
     end
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
   private
-  
+
+  def genre_params
+    params.require(:genre).permit(:nname)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :genres, only: [:index, :create, :edit, :update, :destroy]
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update, :destroy]
+  end
 end
