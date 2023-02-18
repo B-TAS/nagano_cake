eaca9507e0aa8a4899c02e3eb17299e07670489a
kellylayn <kellyrabbit1@gmail.com>
2023-02-18 10:28:34 +0000

    [2/18]4views created

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :items,only: [:index, :new, :create, :show, :edit, :update]
+  end
 end
