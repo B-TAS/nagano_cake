53c0ce11d54e08597dd47eb4f8fab6b5fee92f86
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-02-19 04:18:30 +0000

    add admin

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -26,7 +26,7 @@ class Admin::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 protected
-def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(resource)
     admin_top_path
   end
   
