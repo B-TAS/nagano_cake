b101cbfe281a7ca450926bc5cd92545e49ee16be
kellylayn <kellyrabbit1@gmail.com>
2023-02-19 12:11:44 +0000

    [2/19]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,12 +10,18 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
   end
 
+  private
+  def item_params
+    params.require(:list).permit(:name, :introduction, :genre_id, :genre_id, :price, :is_active)
+  end
 end
