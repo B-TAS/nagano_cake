0e9af651790d96ba96fcefa01427d6a307d5c598
ayaquro <121092738+ayaquro@users.noreply.github.com>
2023-02-19 12:26:53 +0000

    20230219 21:22 管理者ログインページ　TOPページ　ログアウトボタン追加 ルーティング追記

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -29,7 +29,7 @@ protected
   def after_sign_in_path_for(resource)
     admin_top_path
   end
-  
+
   def after_sign_out_path_for(resource)
     new_admin_session_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    #get 'homes/top' => 'homes#top', as: 'homes'
+    get 'top' => 'homes#top', as: 'top'
+  end
 end
