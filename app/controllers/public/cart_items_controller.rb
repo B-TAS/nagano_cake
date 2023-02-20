66801d6439c88b03d17cedf827c1b774e9b7f3fe
ailovemoney <119666255+ailovemoney@users.noreply.github.com>
2023-02-20 08:17:07 +0000

    [ADD]カートフォルダ作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,17 @@
 class Public::CartItemsController < ApplicationController
+  def index
+  end
+  
+  def create
+    
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  def destroy_all
+  end
+  
 end
