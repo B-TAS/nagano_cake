7e9bad3e295a5d4cbba602606737113c2345c639
ailovemoney <119666255+ailovemoney@users.noreply.github.com>
2023-02-20 04:49:46 +0000

    [ADD]ジャンル1ndex,edit編集

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -8,7 +8,7 @@ class Admin::GenresController < ApplicationController
   def create
     @genre = Genre.new(genre_params)
     if @genre.save
-      redirect_to repuest.referer
+      redirect_to request.referer
     else
       @genres = Genre.all
       render 'index'
@@ -16,18 +16,30 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    genre = Genre.find(params[:id])
+    if genre.update(genre_params)
+      redirect_to admin_genres_path
+    else
+      @genre = Genre.find(params[:id])
+      @genre.update(genre_params)
+      render "edit"
+    end
   end
 
   def destroy
+    genre = Genre.find(params[:id])
+    genre.destroy
+    redirect_to admin_genres_path
   end
 
   private
 
   def genre_params
-    params.require(:genre).permit(:nname)
+    params.require(:genre).permit(:name)
   end
 
 end
