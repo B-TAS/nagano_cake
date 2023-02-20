5008d09e9c1d28e3d71dcc12da86e2ba2f0e4bbe
Mouthvalley <45648573+Mouthvalley@users.noreply.github.com>
2023-02-20 04:52:18 +0000

    会員一覧と詳細画面View

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :customers
+  end
 end
