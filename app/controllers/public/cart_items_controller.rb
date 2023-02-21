601b6d1ef3ea87f0d03b8703616be0d24a450e83
ailovemoney <119666255+ailovemoney@users.noreply.github.com>
2023-02-21 13:46:42 +0000

    [ADD]カート機能追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,9 +5,17 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @add_item = CartItem.new(cart_item_params)
-    @add_item.save!
-    redirect_to cart_item_path
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    else
+      @cart_item.save
+      redirect_to cart_items_path
+    end
   end
 
   def update
