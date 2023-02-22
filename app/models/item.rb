0e028f1f8c035033ef3ae2503a1c139f6411769b
kellylayn <kellyrabbit1@gmail.com>
2023-02-22 08:13:13 +0000

    [2/22]change

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :genre
   has_one_attached :image
-  enum is_active: { in_store_now: 0, sold_out: 1 }
+
+  # validates :name, presence: true #<=他カラムの記述
+  validates :is_active, inclusion: { in: [true, false] }
 
   def get_image
     (image.attached?) ? image : 'no_image.jpg'
