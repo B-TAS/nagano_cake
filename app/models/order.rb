7e6e0dbd85a6027a32beaf1c7fe8daac843d0f53
muramasa <121739603+kellylayn@users.noreply.github.com>
2023-02-23 23:02:02 +0900

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,9 +2,4 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
   enum order_method: { credit_card: 0, transfer: 1 }
-
-  # 小計を求めるメソッド
-  def subtotal
-    order.total_price * postage
-  end
 end
