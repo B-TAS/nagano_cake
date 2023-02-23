3f6620f23de49757e59b9aecf8443c43c690385c
muramasa <121739603+kellylayn@users.noreply.github.com>
2023-02-23 23:09:21 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,16 +5,13 @@ class Item < ApplicationRecord
   # cart_itemを通してcustomerに接続
   has_many :customers, through: :cart_items
   has_one_attached :image
-
   has_many :order_detail
-
   # validates :name, presence: true #<=他カラムの記述
   validates :is_active, inclusion: { in: [true, false] }
 
   def get_image
     (image.attached?) ? image : 'no_image.jpg'
   end
-
   # 消費税計算メソッド
   def with_tax_price
     (price*1.1).floor
