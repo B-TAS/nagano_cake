1f1b13a80cf0367b17dfc298f84f40e3d12643e4
Mouthvalley <45648573+Mouthvalley@users.noreply.github.com>
2023-02-24 05:41:23 +0000

    [2/24]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,11 @@
 class Admin::OrdersController < ApplicationController
   def show
-    @orders = Order.all
-    @order_details = OrderDetail.all
+    @orders = Order.find(params[:id])
   end
+
+  # private
+
+  # def customer_params
+  #   params.require(:customer).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :postcode, :address, :phone_number, :email, :is_deleted)
+  # end
 end
